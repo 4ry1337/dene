79fb7e80c0c2d7ad5d9fc1c86e680b9780d4c065
Rakhat Yskak <yskak.rakhat@gmail.com>
2026-05-13 17:22:16 +0500

    feat: Fonts & Splash Screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,24 @@
+import { IBMPlexMono_500Medium, useFonts } from '@expo-google-fonts/ibm-plex-mono';
 import '../global.css'
+import * as SplashScreen from 'expo-splash-screen';
 import { Stack } from "expo-router";
+import * as React from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    IBMPlexMono_500Medium,
+  });
+
+  React.useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   return <Stack />;
 }
